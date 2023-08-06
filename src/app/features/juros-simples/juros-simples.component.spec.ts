8c7f33d98b5cc6162efce3fccf8843cd1dd00ada
Lucas Hayashi <lucashayashi5@gmail.com>
2023-08-06 20:49:22 -0300

    Adicionado cálculo de juros simples

--- src/app/features/juros-simples/juros-simples.component.spec.ts
+++ src/app/features/juros-simples/juros-simples.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JurosSimplesComponent } from './juros-simples.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('JurosSimplesComponent', () => {
   let component: JurosSimplesComponent;
@@ -8,16 +9,63 @@ describe('JurosSimplesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ JurosSimplesComponent ]
+      declarations: [JurosSimplesComponent],
+      imports: [
+        ReactiveFormsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(JurosSimplesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should calculate simple interest by year 1', () => {
+    let formControls = component.jurosCompostosForm.controls;
+    formControls.capitalInicial.setValue("10000");
+    formControls.taxaDeJuros.setValue("3.875");
+    formControls.tempo.setValue("5");
+    formControls.tipoTempo.setValue("anos");
+
+    component.calcularJurosSimples();
+
+    expect(component.montanteFinal).toBe(11937.50);
+  });
+
+  it('should calculate simple interest by month 1', () => {
+    let formControls = component.jurosCompostosForm.controls;
+    formControls.capitalInicial.setValue("10000");
+    formControls.taxaDeJuros.setValue("3.875");
+    formControls.tempo.setValue("60");
+    formControls.tipoTempo.setValue("meses");
+
+    component.calcularJurosSimples();
+
+    expect(component.montanteFinal).toBe(11937.50);
+  });
+
+  it('should calculate simple interest by year 2', () => {
+    let formControls = component.jurosCompostosForm.controls;
+    formControls.capitalInicial.setValue("890200");
+    formControls.taxaDeJuros.setValue("13.25");
+    formControls.tempo.setValue("10");
+    formControls.tipoTempo.setValue("anos");
+
+    component.calcularJurosSimples();
+
+    expect(component.montanteFinal).toBe(2069715.00);
+  });
+
+  it('should calculate simple interest by month 2', () => {
+    let formControls = component.jurosCompostosForm.controls;
+    formControls.capitalInicial.setValue("890200");
+    formControls.taxaDeJuros.setValue("13.25");
+    formControls.tempo.setValue("120");
+    formControls.tipoTempo.setValue("meses");
+
+    component.calcularJurosSimples();
+
+    expect(component.montanteFinal).toBe(2069715.00);
   });
 });
--- src/app/features/juros-simples/juros-simples.component.ts
+++ src/app/features/juros-simples/juros-simples.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexTitleSubtitle, ApexXAxis } from 'ng-apexcharts'
 
 @Component({
   selector: 'app-juros-simples',
@@ -7,4 +9,117 @@ import { Component } from '@angular/core';
 })
 export class JurosSimplesComponent {
 
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis
+  title: ApexTitleSubtitle;
+  dataLabels: ApexDataLabels;
+  dataJuros: Array<any> = [];
+
+  public jurosCompostosForm = this.fb.group({
+    capitalInicial: ['10000', Validators.required],
+    tempo: ['12', Validators.required],
+    tipoTempo: ['meses', Validators.required],
+    taxaDeJuros: ['10', Validators.required]
+  });
+
+  private initializeChartOptions() {
+    this.series = [];
+
+    this.chart = {
+      type: 'bar'
+    };
+
+    this.title = {
+      text: 'Juros simples'
+    };
+
+    this.dataLabels = {
+      enabled: false
+    }
+
+    this.xaxis = {
+      labels: {
+        show: false
+      }
+    }
+  }
+
+  public montanteFinal: any = 0;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initializeChartOptions();
+  }
+
+  getTaxaMensal(taxaDeJuros: number): number {
+    let taxaEquivalente = taxaDeJuros/12;
+    taxaEquivalente = (taxaEquivalente * 100) / 100;
+    return taxaEquivalente;
+  }
+
+  calcularTotalDeJuros(capitalInicial: number, taxaDeJuros: number, tempo: number): number {
+    let totalDeJuros = capitalInicial * taxaDeJuros * tempo;
+    return Number(totalDeJuros.toFixed(2));
+  }
+
+  calcularMontante(capital: number, totalDeJuros: number) {
+    return capital + totalDeJuros;
+  }
+
+  calcularJurosSimples(): void {
+    const jurosForm = this.jurosCompostosForm.value;
+    const capitalInicial = Number(jurosForm.capitalInicial);
+    const taxaDeJuros = Number(jurosForm.taxaDeJuros) / 100;
+    const tipoTempo = jurosForm.tipoTempo;
+    let tempo = Number(jurosForm.tempo);
+
+    if (tipoTempo == "anos") {
+      tempo = tempo * 12;
+    }
+
+    if (this.dataJuros.length) {
+      this.dataJuros = [];
+    }
+    let taxaEquivalente = this.getTaxaMensal(taxaDeJuros)
+    let totalJuros = this.calcularTotalDeJuros(capitalInicial, taxaEquivalente, tempo);
+    this.montanteFinal = this.calcularMontante(capitalInicial, totalJuros);
+    let montanteAtual = capitalInicial;
+    let jurosMensal = totalJuros / tempo;
+
+    for (let i = 0; i <= tempo; i++) {
+      let montanteComJuros: any = 0;
+      let totalDeJurosNoMes: any = 0;
+      let ultimoMontante: any = montanteAtual;
+
+      if (i === 0) {
+        montanteComJuros = capitalInicial
+      } else {
+        montanteAtual += jurosMensal;
+        montanteComJuros = montanteAtual.toFixed(2);
+      }
+
+      totalDeJurosNoMes = (montanteAtual - ultimoMontante).toFixed(2);
+
+      this.dataJuros.push({
+        montanteComJuros,
+        montanteSemJuros: capitalInicial,
+        totalDeJurosNoMes
+      })
+    }
+
+    if (this.series.length) {
+      this.series = [];
+    }
+
+    this.series.push(
+      {
+        name: 'Montante sem Juros',
+        data: this.dataJuros.map(juros => juros.montanteSemJuros)
+      }, {
+      name: 'Montante com Juros',
+      data: this.dataJuros.map(juros => juros.montanteComJuros)
+    });
+  }
 }
